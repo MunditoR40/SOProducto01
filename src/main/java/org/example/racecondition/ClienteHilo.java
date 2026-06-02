c1fd1403ff7e147df2615d60eeb667d7786b2677
MunditoR40 <mundito.egge40@gmail.com>
2026-06-01 21:43:18 -0500

    RaceCondition V1.2

--- src/main/java/org/example/racecondition/ClienteHilo.java
+++ src/main/java/org/example/racecondition/ClienteHilo.java
@@ -13,7 +13,7 @@ public class ClienteHilo implements Runnable {
     public void run() {
         // Cada hilo intentará hacer 3 retiros de 10 dólares
         for (int i = 0; i < 3; i++) {
-            cuenta.retirar(5, nombre);
+            cuenta.retirar(10, nombre);
             try {
                 Thread.sleep(50); // Pausa entre retiros
             } catch (InterruptedException e) {
